e77741b7592d914183f8aa5bf2cd9b87a16f9711
Pushkar Raj <px86@protonmail.com>
2024-10-30 18:54:35 +0530

    Implement controller for service-a

--- service-a/src/main/java/io/github/px86/a/ServiceAApplication.java
+++ service-a/src/main/java/io/github/px86/a/ServiceAApplication.java
@@ -1,13 +1,32 @@
 package io.github.px86.a;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClient;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/api/v1/")
 public class ServiceAApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServiceAApplication.class, args);
-	}
+  @Value("${service-b.url}")
+  private String serviceBUrl;
 
+  @Autowired private RestClient restClient;
+
+  public static void main(String[] args) {
+    SpringApplication.run(ServiceAApplication.class, args);
+  }
+
+  @GetMapping("/")
+  public String home() {
+    String resposne =
+        restClient.get().uri(serviceBUrl + "/api/v1/resource").retrieve().body(String.class);
+    return "Response from Service-B: " + resposne;
+  }
 }
