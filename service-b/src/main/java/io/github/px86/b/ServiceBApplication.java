56b6c5ecdf045d555f33b75c11ee24ef696e4cfa
Pushkar Raj <px86@protonmail.com>
2024-10-30 17:17:28 +0530

    Add spring boot projects

---
+++ service-b/src/main/java/io/github/px86/b/ServiceBApplication.java
@@ -0,0 +1,13 @@
+package io.github.px86.b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceBApplication.class, args);
+	}
+
+}
---
+++ service-b/src/test/java/io/github/px86/b/ServiceBApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.px86.b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
