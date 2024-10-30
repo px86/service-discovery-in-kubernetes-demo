b5f4f94a7c87034e883e667ef4345f0be68687a1
Pushkar Raj <px86@protonmail.com>
2024-10-30 17:27:08 +0530

    Implement Service-B controller

--- service-b/src/main/java/io/github/px86/b/ServiceBApplication.java
+++ service-b/src/main/java/io/github/px86/b/ServiceBApplication.java
@@ -2,12 +2,21 @@ package io.github.px86.b;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/api/v1/")
 public class ServiceBApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServiceBApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(ServiceBApplication.class, args);
+  }
 
+  @GetMapping("/resource")
+  public String getMessage() {
+    return "Hi there! I am Service-B.";
+  }
 }
